b2cafe16b6c141d3f58fad7c93d6dc965156c577
shahketul11 <ketulshah390@gmail.com>
2025-04-15 15:48:14 -0400

    change

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os  # ✅ Required for path handling
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@host.docker.internal:5433/ecommerce'
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@localhost:5433/ecommerce
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@host.docker.internal:5433/ecommerce'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@localhost:5433/ecommerce'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'supersecretkey'
     UPLOAD_FOLDER = os.path.join(basedir, 'static/uploads')
